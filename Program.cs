3f260575c972ce257acc01f9bd1374c92f0d518e
hikmatunnisarangkuti11 <hikmatunnisa72@gmail.com>
2025-05-25 20:40:21 +0700

    push project2

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //numbers = {1, 2, 3, 4, 5} -> target = 4
+        //numbers = {2, 4, 3} -> target = 4
+        //numbers = {1, 5, 8, 9}; -> target = 2
+        int[] numbers = {1, 5, 8, 9};
+        int target = 4;
+
+        bool found = false;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == target)
+            {
+                Console.WriteLine($"Target has been reached in index number {i}");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Target not found in array data");
+        }
+    }
+}
